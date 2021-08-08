4d8792e94649dc319a5db7d7bfb91b7b968780f0
Ewowers <84778797+Ewowers@users.noreply.github.com>
2021-08-09 04:37:08 +0600

    three  commits

--- route/service.api.js
+++ route/service.api.js
@@ -4,6 +4,16 @@ const router = Router();
 router.get("/", control.get); //get all
 router.get("/id=:id", control.getId); // get id
 router.get("/type=:type", control.getType); //type
+// type:
+// psc = цон
+// сfs = азс
+// tourist_agency = турестические агенства
+// shop = магазин
+// cafe = кафе
+// restaurant = ресторан
+// shopping_center = торговый центр
+// cgfs = агэс
+// sport_complex = спортивный комплекс
 router.post("/add", control.add); //add
 router.delete("/:id", control.delete); // delete
 module.exports = router;
