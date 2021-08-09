071ec3a0e8fedb5588f0bd14e5ebe24a96f9c61b
Ewowers <84778797+Ewowers@users.noreply.github.com>
2021-08-10 00:24:18 +0600

    y first commi11

--- route/event.api.js
+++ route/event.api.js
@@ -5,5 +5,5 @@ const router = Router();
 router.get("/", control.get); // получение всего бд
 router.get("/id=:id", control.getOne); // получение по id
 router.get("/date=:date", control.getDate); // получение по дате
-router.post("/", control.add); // создание
+router.post("/add", control.add); // создание
 module.exports = router;
