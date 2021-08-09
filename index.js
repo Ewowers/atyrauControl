a38399cde63d8299ca9fe66c6897f6a7386fbd84
Ewowers <84778797+Ewowers@users.noreply.github.com>
2021-08-10 00:02:49 +0600

    y first commi1

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ app.use("/api", api);
 const start = async () => {
   try {
     //const url = "mongodb://localhost:27017/Atyrau"; // ссылка к базе
-    const urlProduction =
-      "mongodb+srv://Ewower:Ewower12@cluster0.upmbf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    const urlProduction = "mongodb+srv://Ewower:Ewower12@cluster0.upmbf.mongodb.net/Atyrau";
     await mongoose.connect(urlProduction, {
       // подключение к монго
       useNewUrlParser: true,
